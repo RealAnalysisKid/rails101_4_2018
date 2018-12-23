5f0527a1cb574c46b8e5c8a817188a9b891fa98c
Xin <Detective_boys@hotmil.com>
2018-12-23 23:22:39 +0800

    add post button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
